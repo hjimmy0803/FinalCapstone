7ee52c56befc44b20f557116af91d90d8f9c99c7
unknown <hjimmy0803@gmail.com>
2020-03-18 14:33:29 -0700

    Added customer and address model.

---
+++ IndividualCapstone/Models/Address.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IndividualCapstone.Models
+{
+    public class Address
+    { 
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Street")]
+        public string Street { get; set; }
+
+        [Display(Name = "City")]
+        public string City { get; set; }
+
+        [Display(Name = "State")]
+        public string State { get; set; }
+
+        [Display(Name = "Zip Code")]
+        public string ZipCode { get; set; }
+    }
+}
--- IndividualCapstone/Models/Customer.cs
+++ IndividualCapstone/Models/Customer.cs
@@ -1,11 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 
 namespace IndividualCapstone.Models
 {
     public class Customer
     {
+        internal Customer id;
+
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display (Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [ForeignKey("IdentityUSer")]
+        public string IdentityUserId { get; set; }
+        public IdentityUser IdentityUser { get; set; }
+
+
     }
 }
