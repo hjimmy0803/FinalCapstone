408a3dce1d5b8feebc53db7b83e99816e9845703
unknown <hjimmy0803@gmail.com>
2020-03-18 13:48:12 -0700

    Created the register and login roles,

---
+++ IndividualCapstone/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using IndividualCapstone.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(IndividualCapstone.Areas.Identity.IdentityHostingStartup))]
+namespace IndividualCapstone.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
